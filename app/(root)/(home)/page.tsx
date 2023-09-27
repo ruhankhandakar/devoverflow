63021cccbf8064b7d3d49a618479a3b9f90bf2d7
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-09-27 20:37:57 +0600

    fix: ask question link

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-        <Link href="/ask-questions" className="flex justify-end max-sm:w-full">
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a question
           </Button>
